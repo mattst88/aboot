958d0e72607b814a951b9a85865c7e103501831f
Will Woods <wgwoods@users.sourceforge.net>
2001-10-26 23:08:22 +0000

    Added SRM HOWTO, bumped version number to 0.9, minor README and INSTALL adjustments

--- include/config.h
+++ include/config.h
@@ -2,8 +2,8 @@
 #define config_h
 
 #define ABOOT_MAJOR	0
-#define ABOOT_MINOR	8
-#define ABOOT_VERSION	"0.9pre1"
+#define ABOOT_MINOR	9
+#define ABOOT_VERSION	"0.9"
 #define ABOOT_MAGIC	0x30302d746f6f6261 /* "aboot-00" */
 
 #define CONFIG_FILE_PARTITION	1
