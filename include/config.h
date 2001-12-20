94665b6dbb9e54fe19f7b3d8281e5dac7d80fcb9
Will Woods <wgwoods@users.sourceforge.net>
2001-12-20 18:22:11 +0000

    changed version to 0.9a

--- include/config.h
+++ include/config.h
@@ -3,7 +3,7 @@
 
 #define ABOOT_MAJOR	0
 #define ABOOT_MINOR	9
-#define ABOOT_VERSION	"0.9"
+#define ABOOT_VERSION	"0.9a"
 #define ABOOT_MAGIC	0x30302d746f6f6261 /* "aboot-00" */
 
 #define CONFIG_FILE_PARTITION	1
